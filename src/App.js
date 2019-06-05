040bc8fd0655215d820a7ea6f4847ba62d89d512
Rupert Barton <rbarton@scottlogic.com>
2019-06-05 16:12:49 +0100

    Added app.js original garbage back

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import Counter from './Counter'
 function App() {
   return (
     <div className="App">
+            <header className="App-header">	
+        <img src={logo} className="App-logo" alt="logo" />	
+        <p>	
+          Edit <code>src/App.js</code> and save to reload.	
+        </p>	
+        <a	
+          className="App-link"	
+          href="https://reactjs.org"	
+          target="_blank"	
+          rel="noopener noreferrer"	
+        >	
+          Learn React	
+        </a>	
+      </header>
       <Counter />
     </div>
- 
  );
 }
 
